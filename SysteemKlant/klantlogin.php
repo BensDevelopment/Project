905f2e86eb65fba0269ae5d6f0a50c56ab016614
THUIS <THUIS@145.44.80.88>
2015-01-08 09:04:33 +0100

    Merge origin/master
    Conflicts:
    	SysteemAdmin/AdminFactuurOverzicht.php
    	SysteemAdmin/stijl.css

--- SysteemKlant/klantlogin.php
+++ SysteemKlant/klantlogin.php
@@ -19,70 +19,71 @@
                 </div>
             </header>
             <div id="content">
-                <h1>login</h1>                
-                <div class="login">                        
-                    <form action="klantlogin.php" method="POST">
-                        <label>Gebruikersnaam:</label><br>
-                        <input type="text" name="username">
-                        <br>
-                        <label>Wachtwoord:</label><br>
-                        <input type="password" name="password">
-                        <br>
-                        <input type="submit" name="login" value="login">
-                        <br><br>
-                        <a href="klantwwvergeten.php">Wachtwoord vergeten?</a>
-                    </form>
-                </div>     
-                <?php
-                session_start(); //start sessie
-                include "link.php"; //Database connectie
-                if (isset($_POST["login"])) //Hier wordt gecontroleerd of de login knop ingedrukt is.
-                {
-                    $username = $_POST["username"];
-                    $password = $_POST["password"];
-                    $login = $_POST["login"];
-                    if (empty($username) || empty($password) || empty($username) && empty($password))// Deze if loop controleerd of alle velden zijn ingevuld
-                    {
-                        $error = "<p class='foutmelding'>Uw Gebruikersnaam en/of Wachtwoord is niet correct.</p>";
-                        echo $error;
-                    }
-                    else
-                    {
-                        if (isset($login)) //met de volgend if loop wordt bepaald of er goed is ingelogd.
-                        {
-                            $username = $_POST["username"];
-                            $password = $_POST["password"];
-                            $result = mysqli_query($link, "SELECT mail, password FROM User WHERE mail='$username' AND password='$password'");
-                            $rows = mysqli_num_rows($result);
-                            if ($rows == 1)
-                            {
-                                $_SESSION['username'] = $_POST['username'];
-                                $_SESSION['password'] = $_POST['password'];
-                                $_SESSION['login'] = 1;
-                                mysqli_close($link);
-                                include "link.php"; //Deze query zet de status van de gebruiker op online.
-                                $updatelogin = mysqli_prepare($link, "UPDATE User SET status='Online', laatste_inlog=NOW() WHERE mail='$username'");
-                                mysqli_stmt_execute($updatelogin);
-                                header("location: klantoverzicht.php");
-                            }
-                            else
-                            {
-                                $error = "<p class='foutmelding'>Uw Gebruikersnaam en/of Wachtwoord is niet correct.</p>";
-                                echo $error;
-                            }
-                        }
-                    }
-                }
-                if (isset($_GET["link"])) //Deze if loop bekijkt of de gebruiker van een uitlogknop komt, en zet de status van de gebruikker dan op offline
-                {
-                    $username = $_SESSION['username'];
-                    $password = $_SESSION['password'];
-                    $loguit = mysqli_prepare($link, "UPDATE User SET status='Offline' WHERE mail='$username'");
-                    mysqli_stmt_execute($loguit);
-                    session_destroy();
-                    header("location: klantlogin.php");
-                }
-                ?>
+                <h1>login</h1>            	
+            	<div class="login">                    	
+                	<form action="klantlogin.php" method="POST">
+                    	<label>Gebruikersnaam:</label><br>
+                    	<input type="text" name="username">
+                    	<br>
+                    	<label>Wachtwoord:</label><br>
+                    	<input type="password" name="password">
+                    	<br>
+                    	<input type="submit" name="login" value="login">
+                    	<br><br>
+                    	<a href="klantwwvergeten.php">Wachtwoord vergeten?</a>
+                	</form>
+            	</div> 	
+            	<?php
+            	function sha512($password, $unique_salt) {
+                	return crypt($password, '$6$20$' . $unique_salt);
+            	}
+            	function unique_salt() {
+                	return substr(sha1(mt_rand()), 0, 22);
+            	}
+            	session_start(); //start sessie
+            	include "link.php"; //Database connectie
+            	if (isset($_POST["login"])) { //Hier wordt gecontroleerd of de login knop ingedrukt is.
+                	
+                	$username = $_POST["username"];
+                	$password = $_POST["password"];
+                	$hash = sha512($password, $unique_salt); //Hiermee wordt het ingevoerde wachtwoord omgezet in een Hash
+                	$login = $_POST["login"];
+                	if (empty($username) || empty($password) || empty($username) && empty($password)) {// Deze if loop controleerd of alle velden zijn ingevuld
+                    	$error = "<p class='foutmelding'>Uw Gebruikersnaam en/of Wachtwoord is niet correct.</p>";
+                    	echo $error;
+                	} else {
+                    	if (isset($login)) {//met de volgend if loop wordt bepaald of er goed is ingelogd.
+                        	$username = $_POST["username"];
+                        	$password = $_POST["password"];
+                        	$hash = sha512($password, $unique_salt);
+                        	$result = mysqli_query($link, "SELECT mail, password FROM User WHERE mail='$username' AND password='$hash'");
+                        	$rows = mysqli_num_rows($result);
+                        	if ($rows == 1) {
+                            	$_SESSION['username'] = $_POST['username'];
+                            	$_SESSION['password'] = $_POST['password'];
+                            	$_SESSION['login'] = 1;
+                            	mysqli_close($link);
+                            	include "link.php"; //Deze query zet de status van de gebruiker op online.
+                            	$updatelogin = mysqli_prepare($link, "UPDATE User SET status='Online', laatste_inlog=NOW() WHERE mail='$username'");
+                            	mysqli_stmt_execute($updatelogin);
+                            	header("location: klantoverzicht.php");
+                        	} else {
+                            	$error = "<p class='foutmelding'>Uw Gebruikersnaam en/of Wachtwoord is niet correct.</p>";
+                            	echo $error;
+                        	}
+                    	}
+                	}
+            	}
+            	if (isset($_GET["link"])) {
+                	$username = $_SESSION['username'];
+                	$password = $_SESSION['password'];
+                	$loguit = mysqli_prepare($link, "UPDATE User SET status='Offline' WHERE mail='$username'");
+                	mysqli_stmt_execute($loguit);
+                	session_destroy();
+                	header("location: klantlogin.php");
+            	}
+            	?>
+
             </div>
         </div>
         <footer>
--- SysteemKlant/klantwwvergeten.php
+++ SysteemKlant/klantwwvergeten.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!-- Sander van der Stelt, Léyon Courtz -->
+<!-- Sander van der Stelt, Léyon Courtz, Daan Hagemans -->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -20,82 +20,89 @@
             </header>
             <div id="content">
                 <h1>Wachtwoord vergeten</h1><br>
-                <form action="klantwwvergeten.php" method="POST">
-                    <label>E-mail:</label><br>
-                    <input type="text" name="email">
-                    <br>
-                    <input type="submit" name="wwaanvragen" value="wachtwoord aanvragen">
-                </form>
-                <?php
-                include "link.php"; //Connectie wordt het gemaakt.
-
-                if (isset($_POST['wwaanvragen']))
-                {
-                    $email = $_POST['email']; //Hier komt de waarde die ingevuld is door bezoeker
-                    
-                    $stmt = mysqli_prepare($link, "SELECT first_name,mail FROM user WHERE mail = ?"); //Sql beveiligd tegen injecties.
-                    
-                    mysqli_stmt_bind_param($stmt, "s", $email); 
-                    mysqli_stmt_execute($stmt);
-                    mysqli_stmt_bind_result($stmt, $first_name, $email); //Voornaam (die correspondeert met de juiste e-mail wordt hier opgehaald.
-                    mysqli_stmt_store_result($stmt);
-                    
-                    $vind = mysqli_stmt_num_rows($stmt);
-
-                    if ($vind) // Als het ingevoerde e-mail adres overeenkomt met de database (aan de hand van de bovenstaande query) gaat hij de funtie(function) in.
-                    {
-                        function makepassword($length) //Functie heet makepassword
-                        {
-                            $validCharacters = "abcdefghijklmnopqrstuvwxyz123456789"; //De characters waarmee een wachtwoord wordt gemaakt.
-                            $validCharNumber = strlen($validCharacters); //Hier worden het aantal characters uit de string getelt. 
-                            $result = ''; //Resultaat wordt later in de funtie ingevuld.
-                            for ($i = 0; $i < $length; $i++)
-                            {
-                                $result .= $validCharacters[mt_rand(0, $validCharNumber - 1)]; //Willekeuringe selectie wordt hier gemaakt.
-                            }
-                            return $result; //Hier wordt het resultaat terug gegeven.
-                        }
-                        $random_password = makepassword(10); //de variabel krijgt een willekeurige waarde met 10 characters.
-                        $final_result = mysqli_query($link, "UPDATE user SET password ='$random_password' WHERE mail = '$email' "); //willekeuringe waarde word opgeslagen.
-                        if ($final_result) //Als de willekeurige waarde is gemaakt en opgeslagen...
-                        {
-                             while (mysqli_stmt_fetch($stmt)) { //De naam die correspondeert met het e-mail adres wordt opgezocht.
-                                 $naam = $first_name;
-                             }
-                            echo "<p class='succesmelding'>" . "Beste<b> " .$naam. " </b>uw wachtwoord is gewijzigd, <br>bekijk uw e-mail.<br>" . $random_password . "</p>"; //LET OP: $random_password moet er later uit worden gehaald.
-                            /*
-                             * Dit is het mail bericht wat naar de bezoeker wordt gestuurd (local kan niet).
-                             * 
-                            $to = $email;
-                            $subject = 'Uw nieuwe wachtwoord';
-                            $message = '
-                            <html>
-                            <head>
-                              <title>Uw wachtwoord is gewijzigd.</title>
-                            </head>
-                            <body>
-                              <p>Beste begruiker uw nieuwe wachtwoord is:<br>'.$random_password.'</p> $random_password wordt in de bovenstaande code opgehaald.
-                              <p>Met vriendelijke groet,<br> Bens Development</p>
-                            </body>
-                            </html>
-                            ';
-                            $headers  = 'MIME-Version: 1.0' . "\r\n";
-                            $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-                            $headers .= 'From: <support@bensdevelopment.nl>' . "\r\n"; //support is de juiste mail van Bens Development.
-                            $headers .= 'Cc:'. $email . "\r\n"; //$email wordt in de bovenstaande code opgehaald.
-
-                            mail($to, $subject, $message, $headers);*/
-                        }
-                    }
-                    else
-                    {
-                        echo "<p class='foutmelding'>Uw e-mail is niet bekend.</p>"; //Deze melding komt als er geen e-mail adres wordt ingevuld en/of die niet overeenkomt met de database gegevens.
-                        mysqli_stmt_free_result($stmt); // resultset opschonen
-                        mysqli_stmt_close($stmt); // statement opruimen
-                        mysqli_close($link); // verbinding verbreken 
-                    }
-                }
-                ?>
+            	<form action="klantwwvergeten.php" method="POST">
+                	<label>E-mail:</label><br>
+                	<input type="text" name="email">
+                	<br>
+                	<input type="submit" name="wwaanvragen" value="wachtwoord aanvragen">
+            	</form>
+            	<?php
+            	
+            	function sha512($password, $unique_salt) {
+                	return crypt($password, '$6$20$' . $unique_salt);
+            	}
+            	function unique_salt() {
+                	return substr(sha1(mt_rand()), 0, 22);
+            	}
+            	
+            	include "link.php"; //Connectie wordt het gemaakt.
+            	if (isset($_POST['wwaanvragen']))
+            	{
+                	$email = $_POST['email']; //Hier komt de waarde die ingevuld is door bezoeker
+                	
+                	$stmt = mysqli_prepare($link, "SELECT first_name,mail FROM user WHERE mail = ?"); //Sql beveiligd tegen injecties.
+                	
+                	mysqli_stmt_bind_param($stmt, "s", $email);
+                	mysqli_stmt_execute($stmt);
+                	mysqli_stmt_bind_result($stmt, $first_name, $email); //Voornaam (die correspondeert met de juiste e-mail wordt hier opgehaald.
+                	mysqli_stmt_store_result($stmt);
+                	
+                	$vind = mysqli_stmt_num_rows($stmt);
+                	if ($vind) // Als het ingevoerde e-mail adres overeenkomt met de database (aan de hand van de bovenstaande query) gaat hij de funtie(function) in.
+                	{
+                    	function makepassword($length) //Functie heet makepassword
+                    	{
+                        	$validCharacters = "abcdefghijklmnopqrstuvwxyz123456789"; //De characters waarmee een wachtwoord wordt gemaakt.
+                        	$validCharNumber = strlen($validCharacters); //Hier worden het aantal characters uit de string getelt.
+                        	$result = ''; //Resultaat wordt later in de funtie ingevuld.
+                        	for ($i = 0; $i < $length; $i++)
+                        	{
+                            	$result .= $validCharacters[mt_rand(0, $validCharNumber - 1)]; //Willekeuringe selectie wordt hier gemaakt.
+                        	}
+                        	return $result; //Hier wordt het resultaat terug gegeven.
+                    	}
+                    	$random_password = makepassword(10); //de variabel krijgt een willekeurige waarde met 10 characters.
+                    	$hash = sha512($random_password, $unique_salt);
+                    	$final_result = mysqli_query($link, "UPDATE user SET password ='$hash' WHERE mail = '$email' "); //willekeuringe waarde word opgeslagen.
+                    	if ($final_result) //Als de willekeurige waarde is gemaakt en opgeslagen...
+                    	{
+                         	while (mysqli_stmt_fetch($stmt)) { //De naam die correspondeert met het e-mail adres wordt opgezocht.
+                             	$naam = $first_name;
+                         	}
+                        	echo "<p class='succesmelding'>" . "Beste<b> " .$naam. " </b>uw wachtwoord is gewijzigd, <br>bekijk uw e-mail.<br>" . $random_password . "</p>"; //LET OP: $random_password moet er later uit worden gehaald.
+                        	/*
+                         	* Dit is het mail bericht wat naar de bezoeker wordt gestuurd (local kan niet).
+                         	*
+                        	$to = $email;
+                        	$subject = 'Uw nieuwe wachtwoord';
+                        	$message = '
+                        	<html>
+                        	<head>
+                          	<title>Uw wachtwoord is gewijzigd.</title>
+                        	</head>
+                        	<body>
+                          	<p>Beste begruiker uw nieuwe wachtwoord is:<br>'.$random_password.'</p> $random_password wordt in de bovenstaande code opgehaald.
+                          	<p>Met vriendelijke groet,<br> Bens Development</p>
+                        	</body>
+                        	</html>
+                        	';
+                        	$headers  = 'MIME-Version: 1.0' . "\r\n";
+                        	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                        	$headers .= 'From: <support@bensdevelopment.nl>' . "\r\n"; //support is de juiste mail van Bens Development.
+                        	$headers .= 'Cc:'. $email . "\r\n"; //$email wordt in de bovenstaande code opgehaald.
+                        	mail($to, $subject, $message, $headers);*/
+                    	}
+                	}
+                	else
+                	{
+                    	echo "<p class='foutmelding'>Uw e-mail is niet bekend.</p>"; //Deze melding komt als er geen e-mail adres wordt ingevuld en/of die niet overeenkomt met de database gegevens.
+                    	mysqli_stmt_free_result($stmt); // resultset opschonen
+                    	mysqli_stmt_close($stmt); // statement opruimen
+                    	mysqli_close($link); // verbinding verbreken
+                	}
+            	}
+            	?>
+    	</div>
             </div>
         </div>
         <footer>
